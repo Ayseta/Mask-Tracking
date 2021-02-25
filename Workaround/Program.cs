42baadb078e782421e6cab787408a355a8c6c376
AyseTa <ayseta@windowslive.com>
2021-02-25 17:09:11 +0300

    Maske Takip

---
+++ Workaround/Program.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person1 = new Person();
+            person1.FirstName = "İBNİ";
+            person1.LastName = "SİNA";
+            person1.DateOfBirthYear = 980;
+            person1.NationalIdentity = 123;
+
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+    }
+}
